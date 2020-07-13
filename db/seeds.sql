29c41c3f43e720bf828be4ea63437c46e100de1d
BrandonEJenkins <brandonejenkins@gmail.com>
2020-07-13 13:56:02 -0400

    initial commit

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Turkey Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Veggie Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bison Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Angus Burger', false);
