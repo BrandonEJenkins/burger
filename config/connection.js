0f725ea4b1e37115296a84fc410b25ebcf076ec7
BrandonEJenkins <brandonejenkins@gmail.com>
2020-07-25 15:07:23 -0400

    configured mysql connection

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,16 @@
 var mysql = require('mysql2');
 
-var connection = mysql.createConnection({
+var devConnection = {
     host: 'localhost',
     port: 3306,
     user: 'root',
     password: 'Bjbootcamp1!',
     database: 'burgers_db',
-});
+}
+
+var connection = mysql.createConnection(
+    process.env.JAWSDB_URL || devConnection
+);
 
 connection.connect(function (err) {
     if (err) {
